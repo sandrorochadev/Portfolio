599d6687f27116af69d682684031ab3a407fca46
sandrorochadev <sandrorochadev@gmail.com>
2022-09-16 09:44:22 -0300

    feat: Atualizado componente 'Icons'

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,6 +1,6 @@
 import styles from './styles.module.scss'
 import { FaCss3Alt, FaHtml5, FaReact, FaSass } from "react-icons/fa";
-import { SiJavascript, SiTailwindcss, SiTypescript } from "react-icons/si";
+import { SiJavascript, SiTailwindcss, SiTypescript, SiNodedotjs } from "react-icons/si";
 
 export default function Icons() {
     return (
@@ -8,8 +8,9 @@ export default function Icons() {
             <li className={styles.orange}><FaHtml5/></li>
             <li className={styles.blue}><FaCss3Alt/></li>
             <li className={styles.yellow}><SiJavascript/></li>
-            <li className={styles.pink}><FaSass/></li>
             <li className={styles.lightblue}><SiTailwindcss/></li>
+            <li className={styles.pink}><FaSass/></li>
+            <li className={styles.green}><SiNodedotjs/></li>
             <li className={styles.lightblue}><FaReact/></li>
             <li className={styles.blue}><SiTypescript/></li>
         </ul>
