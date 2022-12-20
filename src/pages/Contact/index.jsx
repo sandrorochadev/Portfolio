19155bbed37afaa34c74c73a4028261bf24fbfe7
sandroorochadev <sandrorochadev@gmail.com>
2022-12-20 05:05:42 -0300

    feat: Alteração de conteúdo da página 'Contact'

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
         <div className={styles.container}>
                 <section>
                     <h2>Sobre mim<FiCoffee/></h2>
-                    <p>Sou desenvolvedor web e possuo experiência na criação de sites utilizando a stack HTML, CSS e Javascript, com conhecimento em bibliotecas como o React. Além de novas tecnologias estudo bastante sobre experiência do usuário, acessibilidade e otimização para mecanismos de busca. Possuo graduação em Análise e desenvolvimento de sistemas, e estou realizando transição de carreira para a área de Tecnologia.</p>
+                    <p>Sou desenvolvedor web e possuo experiência na criação de sites utilizando a stack HTML, CSS e Javascript, com conhecimento em bibliotecas como o React. Além de novas tecnologias estudo bastante sobre experiência do usuário, acessibilidade e otimização para mecanismos de busca. Fique a vontade para enviar uma mensagem.</p>
                 </section>
                 <section>
                     <h2>Redes sociais<TbMessageCircle/></h2>
