1c62a353d85e205037aeb448b009508fd7566c34
sandrorochadev <sandrorochadev@gmail.com>
2023-07-23 15:26:57 -0300

    feat: Correções no texto da página 'Home'

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ function Home() {
                 <Icons/>
             </div>
             <div className="container font-light">
-                <p>Sou desenvolvedor web, focado em criar soluções inovadoras e intuitivas. Está página nasceu do objetivo de compartilhar minha experiência e todo conhecimento obtido, através de projetos realizados com tecnologias modernas e frameworks avançados.</p>
+                <p>Sou desenvolvedor web, focado em criar soluções inovadoras e intuitivas. Esta página nasceu do objetivo de compartilhar minha experiência e todo conhecimento obtido, através de projetos realizados com tecnologias modernas e frameworks avançados.</p>
                 <p>Na minha trajetória desenvolvi principalmente habilidades em Javascript, Node, React e outras tecnologias essenciais para a criação de aplicações e websites. Atualmente me dedico bastante em aprender sobre a área de DevOps.</p>
                 <p>Também disponibilizo nesta página uma sessão de links para download de ferramentas para desenvolvedores e entusiastas em tecnologia, além de artigos e vídeos, selecionados para aprimorar sua produtividade e experiência.</p>
             </div>
