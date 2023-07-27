8aff202941d6700a3fb195a4de1a7c37d9799984
sandrorochadev <sandrorochadev@gmail.com>
2023-07-26 21:31:09 -0300

    style: Alteração de estilo do componente 'Cards'

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -10,16 +10,14 @@ export default function Cards() {
     const repositoriesFilter = repositories.filter(item => item.homepage !== null && item.homepage !== '' && item.name !== 'Portfolio')
     
     return (
-        <section className='flex flex-col gap-6'>
+        <section className='flex flex-col gap-4 md:w-3/4'>
             { repositoriesFilter.map(repository => {
                     return (
                         <div className='md:h-32 hover:bg-zinc-800 p-4 rounded flex flex-col justify-center' key={repository.id}>
-                            <h2 className='text-lg md:text-2xl font-semibold'>{repository.name}</h2>
-                            <p className='font-light md:text-lg md:mt-2'>{repository.description}
-                                <a className='mx-1' href={`${repository.homepage}`} target="_blank">
-                                    Clique aqui
-                                </a>
-                            </p>
+                            <a className='mx-1' href={`${repository.homepage}`} target="_blank">
+                                <h2 className='text-lg md:text-xl font-semibold'>{repository.name}</h2>
+                            </a>
+                            <p className='font-light md:text-lg md:mt-2'>{repository.description}</p>
                         </div>
                     )
                 })
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 function Menu() {
     return (
     <header className="container h-16 mx-auto flex items-center justify-center">
-        <nav className="flex gap-4 md:gap-6 md:text-lg text-gray-500 font-light">
-            <Link to="/">Sobre</Link>
-            <Link to="/projects">Projetos</Link>
-            <Link to="/links">Links</Link>
+        <nav className="flex md:text-lg text-gray-500 font-light gap-4 md:gap-6">
+            <Link to="/" className="hover:text-white">Sobre</Link>
+            <Link to="/projects" className="hover:text-white">Projetos</Link>
+            <Link to="/links" className="hover:text-white">Links</Link>
         </nav>
     </header>
     );
